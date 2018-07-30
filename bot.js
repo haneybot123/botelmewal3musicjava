55c2a892a39b841ca7565683f7263a5bda70b4ad
haneybot123 <40692777+haneybot123@users.noreply.github.com>
2018-07-30 03:46:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,121 @@ client.on('ready', () => {
   client.user.setStatus("DND")
 });
 
+client.on('message' , async (message) => {
+       if(message.content.startsWith(prefix + "fort")) {
+  const emoji = "1⃣"
+message.channel.send('**اضغط الاموجي للحصول على رتبه**')
+.then( msg => {
+msg.react(emoji).then( r => {
+  const mis = (reaction, user) => reaction.emoji.name === emoji && user.id === message.author.id;
+  const swe = msg.createReactionCollector(mis, { time: 10000 });
+  swe.on('collect', r => {
+
+let role = message.guild.roles.find("name", "✦ Fortnite");
+message.guild.member(message.author).addRole(role);
+
+});
+});
+});
+}
+});
+
+client.on('message' , async (message) => {
+       if(message.content.startsWith(prefix + "min")) {
+  const emoji = "2⃣"
+message.channel.send('**اضغط الاموجي للحصول على رتبه**')
+.then( msg => {
+msg.react(emoji).then( r => {
+  const mis = (reaction, user) => reaction.emoji.name === emoji && user.id === message.author.id;
+  const swe = msg.createReactionCollector(mis, { time: 10000 });
+  swe.on('collect', r => {
+
+let role = message.guild.roles.find("name", "✦ Minecraft");
+message.guild.member(message.author).addRole(role);
+
+});
+});
+});
+}
+});
+
+
+client.on('message' , async (message) => {
+       if(message.content.startsWith(prefix + "black")) {
+  const emoji = "3⃣"
+message.channel.send('**اضغط الاموجي للحصول على رتبه**')
+.then( msg => {
+msg.react(emoji).then( r => {
+  const mis = (reaction, user) => reaction.emoji.name === emoji && user.id === message.author.id;
+  const swe = msg.createReactionCollector(mis, { time: 10000 });
+  swe.on('collect', r => {
+
+let role = message.guild.roles.find("name", "✦ Blacksquad");
+message.guild.member(message.author).addRole(role);
+
+});
+});
+});
+}
+});
+
+client.on('message' , async (message) => {
+       if(message.content.startsWith(prefix + "CS:GO")) {
+  const emoji = "4⃣"
+message.channel.send('**اضغط الاموجي للحصول على رتبه**')
+.then( msg => {
+msg.react(emoji).then( r => {
+  const mis = (reaction, user) => reaction.emoji.name === emoji && user.id === message.author.id;
+  const swe = msg.createReactionCollector(mis, { time: 10000 });
+  swe.on('collect', r => {
+
+let role = message.guild.roles.find("name", "✦ CS:GO");
+message.guild.member(message.author).addRole(role);
+
+});
+});
+});
+}
+});
+
+client.on('message' , async (message) => {
+       if(message.content.startsWith(prefix + "Palad")) {
+  const emoji = "5⃣"
+message.channel.send('**اضغط الاموجي للحصول على رتبه**')
+.then( msg => {
+msg.react(emoji).then( r => {
+  const mis = (reaction, user) => reaction.emoji.name === emoji && user.id === message.author.id;
+  const swe = msg.createReactionCollector(mis, { time: 10000 });
+  swe.on('collect', r => {
+
+let role = message.guild.roles.find("name", "✦ Paladins");
+message.guild.member(message.author).addRole(role);
+
+});
+});
+});
+}
+});
+
+client.on('message' , async (message) => {
+       if(message.content.startsWith(prefix + "PUB")) {
+  const emoji = "6⃣"
+message.channel.send('**اضغط الاموجي للحصول على رتبه**')
+.then( msg => {
+msg.react(emoji).then( r => {
+  const mis = (reaction, user) => reaction.emoji.name === emoji && user.id === message.author.id;
+  const swe = msg.createReactionCollector(mis, { time: 10000 });
+  swe.on('collect', r => {
+
+let role = message.guild.roles.find("name", "✦ PUBG");
+message.guild.member(message.author).addRole(role);
+
+});
+});
+});
+}
+});
+
 
 
 
